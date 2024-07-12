ab71eaba288eb5491c183004231c7bd0d02e19df
emil <edawood997@gmail.com>
2024-07-12 23:52:48 +0300

    isa last edit

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import app from "./server"
 import * as dotenv from 'dotenv'
 dotenv.config()
 import config from './config'
-app.listen(config.PORT, () => console.log(`it's running in port http://localhost:${config.PORT}`))
+app.listen(config.port, () => console.log(`it's running in port http://localhost:${config.port}`))
